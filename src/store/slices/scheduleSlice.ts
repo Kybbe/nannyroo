2bf13f7df3a120af65a56c233ab53cfc55eb19c9
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2023-08-04 16:37:40 +0200

    Update scheduleSlice

--- src/store/slices/scheduleSlice.ts
+++ src/store/slices/scheduleSlice.ts
@@ -1,35 +1,130 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	schedule: {
+	activeSchedule: {
+		_id: "all",
+		title: "All schedules combined",
+		users: {
+			ownerEmail: "",
+			sharingWith: [],
+		},
 		events: [],
 	} as EventStore,
+	activeScheduleType: "all" as "all" | "individual",
+	schedules: {
+		ownerSchedules: [],
+		sharedSchedules: [],
+	} as {
+		ownerSchedules: EventStore[];
+		sharedSchedules: EventStore[];
+	},
 };
 
 const scheduleSlice = createSlice({
 	name: "schedule",
 	initialState,
 	reducers: {
-		setSchedule: (state, action) => {
-			state.schedule = action.payload;
+		setActiveSchedule: (state, action) => {
+			if (action.payload.type === "all") {
+				state.activeSchedule = {
+					_id: "all",
+					title: "All schedules combined",
+					users: {
+						ownerEmail: "",
+						sharingWith: [],
+					},
+					events: state.schedules.ownerSchedules
+						.concat(state.schedules.sharedSchedules)
+						.flatMap(schedule => schedule.events),
+				};
+				state.activeScheduleType = "all";
+			} else if (action.payload.type === "individual") {
+				state.activeSchedule = action.payload.schedule;
+				state.activeScheduleType = "individual";
+			}
+		},
+		updateSchedule: (state, action) => {
+			const ownerOrShared = action.payload.type;
+			if (ownerOrShared === "owner") {
+				const scheduleIndex = state.schedules.ownerSchedules.findIndex(
+					schedule => schedule._id === action.payload.schedule._id
+				);
+				state.schedules.ownerSchedules[scheduleIndex] = action.payload.schedule;
+			} else if (ownerOrShared === "shared") {
+				const scheduleIndex = state.schedules.sharedSchedules.findIndex(
+					schedule => schedule._id === action.payload.schedule._id
+				);
+				state.schedules.sharedSchedules[scheduleIndex] =
+					action.payload.schedule;
+			} else {
+				throw new Error("Invalid schedule type");
+			}
 		},
 		addEvent: (state, action) => {
-			state.schedule.events.push(action.payload);
+			state.activeSchedule.events.push(action.payload);
 		},
 		updateEvent: (state, action) => {
-			const eventIndex = state.schedule.events.findIndex(
+			const flattenedSchedules = [
+				...state.schedules.ownerSchedules,
+				...state.schedules.sharedSchedules,
+			];
+			const parentSchedule = action.payload.parentScheduleId
+				? flattenedSchedules.find(
+						sch => sch._id === action.payload.parentScheduleId
+				  )
+				: flattenedSchedules.find(sch => sch._id === state.activeSchedule._id);
+			if (!parentSchedule) return;
+
+			const eventIndex = parentSchedule.events.findIndex(
 				event => event.id === action.payload.id
 			);
-			state.schedule.events[eventIndex] = action.payload;
+			parentSchedule.events[eventIndex] = action.payload;
 		},
 		deleteEvent: (state, action) => {
-			state.schedule.events = state.schedule.events.filter(
+			state.activeSchedule.events = state.activeSchedule.events.filter(
 				event => event.id !== action.payload
 			);
+		},
+
+		setSchedules: (state, action) => {
+			state.schedules = action.payload;
+		},
+		addSchedule: (state, action) => {
+			const ownerOrShared = action.payload.type;
+			if (ownerOrShared === "owner") {
+				state.schedules.ownerSchedules.push(action.payload.schedule);
+			} else if (ownerOrShared === "shared") {
+				state.schedules.sharedSchedules.push(action.payload.schedule);
+			} else {
+				throw new Error("Invalid schedule type");
+			}
+		},
+		deleteSchedule: (state, action) => {
+			const ownerOrShared = action.payload.type;
+			if (ownerOrShared === "owner") {
+				state.schedules.ownerSchedules = state.schedules.ownerSchedules.filter(
+					schedule => schedule._id !== action.payload.id
+				);
+			} else if (ownerOrShared === "shared") {
+				state.schedules.sharedSchedules =
+					state.schedules.sharedSchedules.filter(
+						schedule => schedule._id !== action.payload.id
+					);
+			} else {
+				throw new Error("Invalid schedule type");
+			}
 		},
 	},
 });
 
-export const { setSchedule, addEvent, updateEvent, deleteEvent } =
-	scheduleSlice.actions;
+export const {
+	setActiveSchedule,
+	updateSchedule,
+	addEvent,
+	updateEvent,
+	deleteEvent,
+	setSchedules,
+	addSchedule,
+	deleteSchedule,
+} = scheduleSlice.actions;
 export default scheduleSlice;
