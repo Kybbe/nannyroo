38a6678b46eb6cdc9bc1e2fe56cbeb5267fa7c3e
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2023-08-04 16:38:05 +0200

    Update profile to firebase auth

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,22 +1,149 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { useAuthContext } from "@/context/AuthContext";
+import signUp, { signIn } from "@/helpers/frontend/firebase/Auth";
+import {
+	AuthError,
+	GoogleAuthProvider,
+	getAuth,
+	sendPasswordResetEmail,
+	signInWithPopup,
+} from "firebase/auth";
+import Link from "next/link";
+import { useState } from "react";
+
+const provider = new GoogleAuthProvider();
 
 export default function Profile() {
-	const { data: session } = useSession();
-	if (!session)
+	const [loginInfo, setLoginInfo] = useState({
+		email: "",
+		password: "",
+	});
+	const [loginOrRegister, setLoginOrRegister] = useState<"Login" | "Register">(
+		"Login"
+	);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<AuthError | null>(null);
+	const user = useAuthContext();
+
+	const loginRegister = async () => {
+		if (loading) return;
+		if (!loginInfo.email || !loginInfo.password) {
+			alert("Please enter an email and password");
+			return;
+		}
+
+		setLoading(true);
+		if (loginOrRegister === "Login") {
+			const { result: res, error: err } = await signIn(
+				loginInfo.email,
+				loginInfo.password
+			);
+			if (err) {
+				setError(err);
+				setLoading(false);
+				return;
+			}
+			console.info("logged in response", res);
+		} else {
+			const { result: res, error: err } = await signUp(
+				loginInfo.email,
+				loginInfo.password
+			);
+			if (err) {
+				setError(err);
+				setLoading(false);
+				return;
+			}
+			console.info("register response", res);
+		}
+		setLoading(false);
+	};
+
+	if (!user)
 		return (
-			<div className="flex flex-col items-center gap-4 mt-24">
-				<h2 className="text-2xl font-bold text-center">
-					You are not logged in!
-				</h2>
+			<div className="flex items-center justify-center flex-1">
+				<div className="flex flex-col items-center gap-4 p-6 bg-white rounded-lg shadow-lg">
+					<h2 className="text-2xl font-bold text-center">
+						{loginOrRegister} to SitterSync
+					</h2>
+
+					<input
+						type="email"
+						placeholder="email"
+						value={loginInfo.email}
+						onChange={e => {
+							setLoginInfo({ ...loginInfo, email: e.target.value });
+						}}
+						className="w-full p-1 border-solid border-b border-primaryDark focus:border-primaryDark"
+					/>
+					<input
+						type="password"
+						placeholder="password"
+						value={loginInfo.password}
+						onChange={e => {
+							setLoginInfo({ ...loginInfo, password: e.target.value });
+						}}
+						className="w-full p-1 border-solid border-b border-primaryDark focus:border-primaryDark"
+					/>
+					<button
+						className={`bg-primaryDark text-primaryBackground rounded-lg px-4 py-2 w-full ${
+							error ? "border-2 border-red-500 bg-red-300 text-neutral-800" : ""
+						} hover:bg-teal-800 hover:text-neutral-100 transition-colors`}
+						type="button"
+						onClick={async () => {
+							loginRegister();
+						}}
+					>
+						{loginOrRegister}
+					</button>
+
+					{error && (
+						<p className="text-red-500">{`${error.code}, ${error.message}`}</p>
+					)}
+
+					<h2 className="text-xl font-bold text-center">OR</h2>
+
+					<button
+						className="bg-primaryDark text-primaryBackground rounded-lg px-4 py-2 w-full hover:bg-teal-800 hover:text-neutral-100 transition-colors"
+						type="button"
+						onClick={async () => {
+							setLoading(true);
+							const auth = getAuth();
+							signInWithPopup(auth, provider)
+								.then(result => {
+									const { user: googleUser } = result;
+									console.info("googleUser", googleUser);
+								})
+								.catch(err => {
+									console.error(err);
+									setError(err);
+								});
+							setLoading(false);
+						}}
+					>
+						Sign in with Google
+					</button>
+
+					<button
+						className="text-primaryDark text-sm mt-4 hover:underline"
+						type="button"
+						onClick={() => {
+							setLoginOrRegister(
+								loginOrRegister === "Login" ? "Register" : "Login"
+							);
+						}}
+					>
+						Switch to {loginOrRegister === "Login" ? "Register" : "Login"}
+					</button>
+				</div>
 			</div>
 		);
 	return (
 		<div className="flex flex-col items-center gap-1">
-			{session.user?.image && (
+			{user.photoURL && (
 				<img
-					src={session.user?.image}
+					src={user.photoURL}
 					alt="Profile pic of user"
 					width={200}
 					height={200}
@@ -24,10 +151,10 @@ export default function Profile() {
 				/>
 			)}
 			<h2 className="text-2xl font-bold text-center">
-				Welcome back, {session.user?.name}!
+				Welcome back, {user.displayName || user.email}!
 			</h2>
 
-			<p className="text-l text-center">{session.user?.email}</p>
+			<p className="text-l text-center">{user?.email}</p>
 
 			<label htmlFor="notis" className="flex flex-row items-center gap-2 mt-4">
 				I would like notfications when tasks are completed by others{" "}
@@ -38,6 +165,31 @@ export default function Profile() {
 					className="cursor-pointer h-4 w-4"
 				/>
 			</label>
+
+			<Link href="/schedule" className="hover:underline">
+				Go to schedule
+			</Link>
+
+			<button
+				className="bg-primaryDark text-primaryBackground rounded-lg px-4 py-2 hover:bg-teal-800 hover:text-neutral-100 transition-colors"
+				type="button"
+				onClick={async () => {
+					if (!user || !user.email) return;
+
+					const auth = getAuth();
+					sendPasswordResetEmail(auth, user.email)
+						.then(() => {
+							// Password reset email sent!
+							// ..
+						})
+						.catch(err => {
+							console.error(err);
+							setError(err);
+						});
+				}}
+			>
+				Reset password
+			</button>
 		</div>
 	);
 }
