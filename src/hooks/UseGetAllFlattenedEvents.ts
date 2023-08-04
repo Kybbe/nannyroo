172bbf72ff6b7cbd9b69bf4c1a626981655856d1
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2023-08-04 16:37:14 +0200

    create hooks to flatten events

---
+++ src/hooks/UseGetAllFlattenedEvents.ts
@@ -0,0 +1,29 @@
+import { useAppSelector } from "@/hooks/redux/useAppSelector";
+import { useEffect, useState } from "react";
+
+export default function UseGetAllFlattenedEvents() {
+	const schedules = useAppSelector(state => state.schedule.schedules);
+	const [flattenedEvents, setFlattenedEvents] = useState<
+		ScheduleEvent[] | undefined
+	>();
+
+	useEffect(() => {
+		const flatSchedules = [
+			...schedules.ownerSchedules,
+			...schedules.sharedSchedules,
+		];
+
+		const events = flatSchedules.map((schedule, si) => {
+			const { events: evs } = schedule;
+			// insert parentScheduleId into each event
+			return evs.map((ev, i) => ({
+				...ev,
+				parentScheduleId: schedule._id,
+			}));
+		});
+
+		setFlattenedEvents(events.flat());
+	}, [schedules.ownerSchedules, schedules.sharedSchedules]);
+
+	return flattenedEvents;
+}
---
+++ src/hooks/UseGetAllWriteableEvents.ts
@@ -0,0 +1,40 @@
+import { useAuthContext } from "@/context/AuthContext";
+import { useEffect, useState } from "react";
+import UseGetAllFlattenedEvents from "./UseGetAllFlattenedEvents";
+import { useAppSelector } from "./redux/useAppSelector";
+
+export default function UseGetAllWriteableEvents() {
+	const flattenedEvents = UseGetAllFlattenedEvents();
+	const user = useAuthContext();
+	const schedules = useAppSelector(state => state.schedule.schedules);
+
+	const [writeableEvents, setWriteableEvents] = useState<
+		ScheduleEvent[] | undefined
+	>();
+
+	useEffect(() => {
+		if (!flattenedEvents || !user) return;
+
+		const writeable = flattenedEvents.filter(event => {
+			const flattenedSchedules = [
+				...schedules.ownerSchedules,
+				...schedules.sharedSchedules,
+			];
+			const parentSchedule = flattenedSchedules.find(
+				sch => sch._id === event.parentScheduleId
+			);
+			if (!parentSchedule) return false;
+
+			return (
+				parentSchedule.users.ownerEmail === user.email ||
+				parentSchedule.users.sharingWith.find(
+					u => u.userEmail === user.email && u.permissions === "write"
+				)
+			);
+		});
+
+		setWriteableEvents(writeable);
+	}, [flattenedEvents, user, schedules]);
+
+	return writeableEvents;
+}
